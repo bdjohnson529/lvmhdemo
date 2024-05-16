dd4b00bf3bb7b79b7e3f4f126e77072be23c452f
Ben Johnson <ben.johnson@retool.com>
2024-05-16 12:59:05 +0000

    change image url
    Edit image_url

---
+++ apps/Sales Dashboard/lib/image_url.js
@@ -0,0 +1,9 @@
+const house = {{ retoolContext.configVars.house_name}}
+
+
+if (house == "Moet") {
+  return '306d16a1-bf8d-4c10-9c64-84746f2dd27e'
+}
+else if (house == "LVMH") {
+  return '7fe1a987-df12-468d-b878-3c58ca4e7e57'
+}
