d68c42017888045a90862d46fa5de882ad109875
Ben Johnson <ben.johnson@retool.com>
2024-05-16 13:09:42 +0000

    add logo
    Edit logo_url

---
+++ apps/Sales Dashboard/lib/logo_url.js
@@ -0,0 +1,12 @@
+const house = {{ retoolContext.configVars.house_name}}
+
+
+if (house == "LVMH") {
+  return '71869c6e-ad92-4f32-864f-14564e05f6af'
+}
+if (house == "Moet") {
+  return 'a4580a22-2535-4e11-a65e-f23e82e765e6'
+}
+if (house == "Hennessy") {
+  return '4f78f6e6-c8aa-45e5-a43f-3746898101c1'
+}
